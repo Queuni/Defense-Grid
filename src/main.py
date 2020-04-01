a0297f067a205ed6c18a91c7b865b34781f4cd5f
Queuni <Queuni@users.noreply.github.com>
2020-04-01 17:45:59 +0000

    Update the changelog with the fixes included in this release

--- src/main.py
+++ src/main.py
@@ -4,3 +4,5 @@
 # Correct the docstring to match the actual behavior of the function
 
 # Adjust the batch size to reduce memory usage on large inputs
+
+# Correct the default path used when no config file is specified
