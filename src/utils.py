c81ccb27dd4c8b56d1151a67c893ab08bf4fb2b1
Queuni <Queuni@users.noreply.github.com>
2020-04-13 19:13:23 +0000

    Bump the CI image to use the latest stable runner version

--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,5 @@
 # Simplify the config merge logic so overrides are predictable
 
 # Correct the comparison that was using the wrong operator
+
+# Correct the default path used when no config file is specified
