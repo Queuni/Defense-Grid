1218d225ec455f8ade8805ef484626f1b6b382ad
Queuni <Queuni@users.noreply.github.com>
2020-05-01 13:23:38 +0000

    Correct the default value for the feature flag in production

--- lib/helpers.py
+++ lib/helpers.py
@@ -6,3 +6,5 @@
 # Simplify the config validation by using a declarative schema
 
 # Correct the timestamp format to use ISO 8601 for consistency
+
+# Add validation for the config schema before applying settings
